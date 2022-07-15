3ec15255a470c1ea900933f91a94241a37595e80
Niel Conradie <nielconradie93@gmail.com>
2022-07-15 16:24:55 +0200

    Update repositories.py
    Working with the Response Dicionary.

--- github-repositories/repositories.py
+++ github-repositories/repositories.py
@@ -9,6 +9,19 @@ print(f"Status code: {request.status_code}")
 
 # Store API response in a variable.
 data = request.json()
+print(f"Total repositories: {data['total_count']}")
 
-# Process results.
-print(data.keys())
+# Explore information about the repositories.
+items = data["items"]
+print(f"Repositories returned: {len(items)}")
+
+# Examine the first repository.
+repository = items[0]
+print("\nSelected information about first repository:")
+print(f"Name: {repository['name']}")
+print(f"Owner: {repository['owner']['login']}")
+print(f"Stars: {repository['stargazers_count']}")
+print(f"Repository: {repository['html_url']}")
+print(f"Created: {repository['created_at']}")
+print(f"Updated: {repository['updated_at']}")
+print(f"Description: {repository['description']}")
