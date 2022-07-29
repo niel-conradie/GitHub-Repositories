86814eb20a5742c9eca6b81475fb38c146162de0
Niel Conradie <nielconradie93@gmail.com>
2022-07-29 13:03:41 +0200

    Update run.py
    Removed old unused code and added start_app() method.

--- github-repositories/run.py
+++ github-repositories/run.py
@@ -2,19 +2,11 @@ from repositories import Repositories
 
 
 def run():
-    """Github-Repositories."""
+    """GitHub-Repositories."""
     run = Repositories()
 
-    # Requesting user input.
-    user_input = run.user_input()
-    # Store data in a variable.
-    data = run.request_data(user_input)
-    # Print data in terminal.
-    run.display_request_data(data)
-    # Store relevant visualization data in variable.
-    links, stars, labels = run.prepare_visualization(data)
-    # Generate visualization and create .html file.
-    run.visualization(user_input, links, stars, labels)
+    # Starting the application.
+    run.start_app()
 
 
 if __name__ == "__main__":
