5bb53d5e2a752a2b7ad2fe2a20dcae944a8a1170
Niel Conradie <nielconradie93@gmail.com>
2023-01-05 13:14:00 +0200

    Update run.py
    Added Keyboard Interrupt exception handeling

--- github-repositories/run.py
+++ github-repositories/run.py
@@ -5,8 +5,12 @@ def run():
     """GitHub-Repositories."""
     run = Repositories()
 
-    # Starting the application.
-    run.start_app()
+    try:
+        # Starting the application.
+        run.start_app()
+    except KeyboardInterrupt:
+        # Stopping the application.
+        quit("\n\nProgram Terminated")
 
 
 if __name__ == "__main__":
